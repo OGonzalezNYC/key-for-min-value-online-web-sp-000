fadff8c0ecf5a1150bbe1f05b35c40dad30c1722
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-06 01:23:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -47,17 +47,17 @@
 #   #sorted_array.first.key
 # end
 
-def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
-  lowest_key
-end
+# def key_for_min_value(hash)
+#   lowest_key = nil
+#   lowest_value = nil
+#   hash.each do |k, v|
+#     if lowest_value == nil || v < lowest_value
+#       lowest_value = v
+#       lowest_key = k
+#     end
+#   end
+#   lowest_key
+# end
 
 def key_for_min_value(hash)
   lowest_key = nil 
